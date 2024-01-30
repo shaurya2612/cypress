f4aa32699e0506466dec61bc04af7bf875e9affd
Shaurya <shaurya@Shauryas-MacBook-Pro.local>
2024-01-30 10:40:42 +0530

    Add header in landing page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,21 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormSchema } from "@/src/lib/types";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/src/components/ui/form";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../../public/cypresslogo.svg";
+import { Input } from "@/src/components/ui/input";
+import Loader from "@/src/components/global/Loader";
+import { Button } from "@/src/components/ui/button";
+import { actionLoginUser } from "@/src/lib/server-action/auth-actions";
 
 const LoginPage: React.FC = () => {
   const router = useRouter();
@@ -12,17 +27,96 @@ const LoginPage: React.FC = () => {
 
   const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
     formData,
-  ) => {};
+  ) => {
+    const { error } = await actionLoginUser(formData);
+    if (error) {
+      form.reset();
+      setSubmitError(error.message);
+    }
+		router.replace('/dashboard');
+  };
 
-  const {
-    formState: { isLoading },
-  } = useForm<z.infer<typeof FormSchema>>({
+  const form = useForm<z.infer<typeof FormSchema>>({
     mode: "onChange",
     resolver: zodResolver(FormSchema),
     defaultValues: { email: "", password: "" },
   });
 
-  return <div>LoginPage</div>;
+  const {
+    handleSubmit,
+    formState: { isLoading },
+  } = form;
+
+  return (
+    <Form {...form}>
+      <form
+        onChange={() => {
+          if (submitError) setSubmitError("");
+        }}
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-full flex-col space-y-6 sm:w-[400px] sm:justify-center"
+      >
+        <Link
+          href="/"
+          className="
+          justify-left
+          flex
+          w-full
+          items-center"
+        >
+          <Image src={Logo} alt="cypress Logo" width={50} height={50} />
+          <span
+            className="text-4xl
+          font-semibold first-letter:ml-2 dark:text-white"
+          >
+            cypress.
+          </span>
+        </Link>
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button type="submit" className="w-full p-6" disabled={isLoading}>
+          {!isLoading ? "Login" : <Loader />}
+        </Button>
+        <span className="self-container">
+          Dont have an account?{" "}
+          <Link href="/signup" className="text-primary">
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </Form>
+  );
 };
 
 export default LoginPage;
--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,11 +1,13 @@
+import Header from '@/src/components/landing-page/header'
 import React from 'react'
 
 const HomePageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
   return (
     <main>
+      <Header/>
       {children}      
     </main>
-  )
+  ) 
 }
 
 export default HomePageLayout
